d8b242719f01e6fef30b81792a0ea7e3f3e02b2a
Iñaki 🎮 Gomez Romero <inaki.gomez.2a@outlook.com>
2025-08-20 15:46:27 -0600

    cambios inicio memorizacion

---
+++ Controllers/MemorizacionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AnzanMegaArithmetics.Controllers
+{
+    public class MemorizacionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
